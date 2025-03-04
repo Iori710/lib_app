b0948a02bc4030b3662f9a58acb4a484342535d1
Iori Ono <irono.j2@kaien-t.net>
2025-03-04 15:12:29 +0900

    DB作成中

--- lib_app/models.py
+++ lib_app/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    user_id = models.IntegerField
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
     email = models.EmailField
     HN = models.CharField(max_length=50)
     PW = models.CharField(max_length=50)
     
-class library(models.Model):
-    book_id = models.IntegerField
-    ISBN = models.BigIntegerField
+class Library(models.Model):
+    ISBN = models.BigIntegerField(primary_key=True)
+    stock = models.IntegerField
+    
+class Book(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    ISBN = models.ForeignKey(Library, on_delete = models.CASCADE)
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=50)
     publisher = models.CharField(max_length=50)
     shelf = models.CharField(max_length=50)
     num_of_books = models.IntegerField
     c_code = models.BigIntegerField
+    
+class Review(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    

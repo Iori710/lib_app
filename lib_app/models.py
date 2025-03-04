3bfded00bc54bde065e0835438b577a0d2d125c5
Iori Ono <irono.j2@kaien-t.net>
2025-03-04 15:21:25 +0900

    DB作成中

--- lib_app/models.py
+++ lib_app/models.py
@@ -6,20 +6,21 @@ class User(models.Model):
     email = models.EmailField
     HN = models.CharField(max_length=50)
     PW = models.CharField(max_length=50)
-    
-class Library(models.Model):
-    ISBN = models.BigIntegerField(primary_key=True)
-    stock = models.IntegerField
+
     
 class Book(models.Model):
     book_id = models.IntegerField(primary_key=True)
-    ISBN = models.ForeignKey(Library, on_delete = models.CASCADE)
+    ISBN = models.BigIntegerField
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=50)
     publisher = models.CharField(max_length=50)
     shelf = models.CharField(max_length=50)
     num_of_books = models.IntegerField
     c_code = models.BigIntegerField
+    
+class Library(models.Model):
+    ISBN = models.ForeignKey(Book.ISBN,primary_key=True)
+    stock = models.IntegerField
     
 class Review(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)

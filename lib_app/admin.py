bffd175f80854d85e8d43537a9a1cbf0806fed00
Iori Ono <irono.j2@kaien-t.net>
2025-03-05 14:10:30 +0900

    DB ver.1.1

--- lib_app/admin.py
+++ lib_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, Reserve, Lending
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Reserve)
+admin.site.register(Lending)
--- lib_app/models.py
+++ lib_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date, timedelta
 
 # Create your models here.
 class User(models.Model):
@@ -10,29 +11,28 @@ class User(models.Model):
     
 class Book(models.Model):
     book_id = models.IntegerField(primary_key=True)
-    ISBN = models.BigIntegerField
+    ISBN = models.BigIntegerField(default=3784000000000)
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=50)
     publisher = models.CharField(max_length=50)
     shelf = models.CharField(max_length=50)
-    num_of_books = models.IntegerField
-    c_code = models.BigIntegerField
+    c_code = models.BigIntegerField(default=1920000000000)
     
 class Library(models.Model):
     ISBN = models.ForeignKey(Book,on_delete=models.CASCADE,primary_key=True)
-    stock = models.IntegerField
+    stock = models.IntegerField(default=1)
     
 class Review(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     ISBN = models.ForeignKey(Library, on_delete=models.CASCADE)
-    stars = models.IntegerField
+    stars = models.IntegerField(default=0)
     review = models.TextField(blank=True)
     
 class Reserve(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-    lending_start = models.DateField
-    lending_end = models.DateField
+    lending_start = models.DateField(default=date.today())
+    lending_end = models.DateField(default=date.today() + timedelta(days=7))
     
 class Lending(Reserve):
     returned = models.BooleanField(default=False)

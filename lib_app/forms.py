e47b916ffb5cbb9c098f7da44a86d25926bc4f57
Iori Ono <irono.j2@kaien-t.net>
2025-03-11 15:10:48 +0900

    DB ver.2.1 書籍登録画面仮作成中

--- lib_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Book
-
-# Create your forms here.
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['ISBN', 'shelf', 'c_code']
